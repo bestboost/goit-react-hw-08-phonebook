66dae791bccad371ded156aee1434588a8c4f654
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2025-03-12 12:43:53 +0200

    change hosting

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://nodejs-homework-rest-api-mspb.onrender.com';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
